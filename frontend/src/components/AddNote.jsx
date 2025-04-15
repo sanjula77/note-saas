f05410149e463af953f53c793c8459b37f53595a
Gihan sanjula <sanjulagihan94@gmail.com>
2025-04-15 14:18:30 +0530

    feat: add deployment workflow and environment configuration files

--- frontend/src/components/AddNote.jsx
+++ frontend/src/components/AddNote.jsx
@@ -1,9 +1,19 @@
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+
+// export default function AddNote() {
+//   return (
+//     <Link to={'/add'}>
+//         <button className='addNote'>Add Note</button>
+//     </Link>
+//   )
+// }
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function AddNote() {
   return (
-    <Link to={'/add'}>
-        <button className='addNote'>Add Note</button>
+    <Link to="/add">
+      <button className="addNote">Add Note</button>
     </Link>
-  )
+  );
 }
